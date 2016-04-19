bbca9c1ff65e6d1bdfb76b66c2be04e2523f94ce
SonyaMoisset <sonya.moisset@gmail.com>
2016-04-19 23:27:33 +0900

    Add app folder

---
+++ app/channels/channels.controller.js
@@ -0,0 +1,35 @@
+angular.module('angularfireSlackApp')
+  .controller('ChannelsCtrl', function($state, Auth, Users, profile, channels){
+    var channelsCtrl = this;
+
+    channelsCtrl.profile = profile;
+    channelsCtrl.channels = channels;
+    channelsCtrl.getDisplayName = Users.getDisplayName;
+    channelsCtrl.getGravatar = Users.getGravatar;
+    channelsCtrl.users = Users.all;
+
+    channelsCtrl.newChannel = {
+      name: ''
+    };
+
+    Users.setOnline(profile.$id);
+
+    channelsCtrl.logout = function(){
+      channelsCtrl.profile.online = null;
+      channelsCtrl.profile.$save().then(function(){
+        Auth.$unauth();
+        $state.go('home');
+      });
+    };
+
+    channelsCtrl.logout = function(){
+      Auth.$unauth();
+      $state.go('home');
+    };
+
+    channelsCtrl.createChannel = function(){
+      channelsCtrl.channels.$add(channelsCtrl.newChannel).then(function(ref){
+        $state.go('channels.messages', {channelId: ref.key()});
+      });
+    };
+  });
